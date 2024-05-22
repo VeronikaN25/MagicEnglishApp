67ca069ae736f620705c47af850784496d16c167
Veeя <novichikhina.05@mail.ru>
2024-05-22 09:42:29 +0500

    json file upload

--- app/src/main/java/com/example/magicenglish/test_trainer/utils/Constants.kt
+++ app/src/main/java/com/example/magicenglish/test_trainer/utils/Constants.kt
@@ -1,5 +1,6 @@
 package com.example.magicenglish.test_trainer.utils
 
 object Constants {
-    const val BASE_URL = "https://raw.githubusercontent.com/ClandEstine47/English-Quiz-App/master/"
+    const val BASE_URL = "https://github.com/VeronikaN25/MagicEnglishApp/master/"
+
 }
